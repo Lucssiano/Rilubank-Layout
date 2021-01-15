409adb4b5a3d7c8e950d818890ed1c7ba70bcc41
Luciano Riente <lucianotomasriente@gmail.com>
2021-01-15 20:45:12 -0300

    Se agrega el contenido principal de la página

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,44 @@
+/* ### VARIABLES #### */
+
+// Botón hamburguesa
 const burguer = document.querySelector(".menu-button");
+// Lista de navegación del header
 const nav = document.querySelector(".ul-list");
+// Header
+const header = document.querySelector(".main-header");
+// Contenedor del header
+const headerContainer = document.querySelector(".header-container");
+// Listas del nav
+const listNav = document.querySelector(".li-header");
 
+/* ### FUNCIONES #### */
 
+// Abrir el menú de navegación en mobile
 burguer.addEventListener("click", () => {
   nav.classList.toggle("burger-action");
 });
 
+// Si se presiona la tecla ESCAPE se cierra el menú de navegación
+function escPushed() {
+  teclaEsc = event.keyCode;
+  if (teclaEsc == 27) {
+    nav.classList.remove("burger-action");
+  }
+}
 
+// Si se presiona en alguna parte que no sea el header se cierra el menú de navegación
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (
+    nav.classList.contains("burger-action") &&
+    e.target != header &&
+    e.target != headerContainer &&
+    e.target != listNav
+  ) {
+    // console.log("hi");
+    nav.classList.remove("burguer-action");
+  }
+});
+
+// Inmediatamente que se presione la tecla ESCAPE se cierra el menú de navegación
+window.onkeydown = escPushed;
