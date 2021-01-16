664fee4e8c204804157c472f3b22c27877964c47
Luciano Riente <lucianotomasriente@gmail.com>
2021-01-16 01:38:08 -0300

    Se mejoró el contenido principal y se agrego funcionalidad al header en mobile

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 /* ### VARIABLES #### */
 
 // Botón hamburguesa
-const burguer = document.querySelector(".menu-button");
+const burger = document.querySelector(".menu-button");
 // Lista de navegación del header
 const nav = document.querySelector(".ul-list");
 // Header
@@ -9,12 +9,12 @@ const header = document.querySelector(".main-header");
 // Contenedor del header
 const headerContainer = document.querySelector(".header-container");
 // Listas del nav
-const listNav = document.querySelector(".li-header");
+const listNav = document.querySelector(".li-header"); // Solo toma la primer lista, las otras no (?)
 
 /* ### FUNCIONES #### */
 
 // Abrir el menú de navegación en mobile
-burguer.addEventListener("click", () => {
+burger.addEventListener("click", () => {
   nav.classList.toggle("burger-action");
 });
 
@@ -31,14 +31,23 @@ window.addEventListener("click", (e) => {
   console.log(e.target);
   if (
     nav.classList.contains("burger-action") &&
-    e.target != header &&
-    e.target != headerContainer &&
-    e.target != listNav
+    e.target !== header &&
+    e.target !== headerContainer &&
+    e.target !== listNav &&
+    e.target !== burger
   ) {
-    // console.log("hi");
-    nav.classList.remove("burguer-action");
+    nav.classList.remove("burger-action");
   }
 });
 
 // Inmediatamente que se presione la tecla ESCAPE se cierra el menú de navegación
 window.onkeydown = escPushed;
+
+// Scroll reveals
+// window.sr = ScrollReveal();
+
+// sr.reveal(".arrow-container", {
+//   duration: 2000,
+//   origin: "bottom",
+//   distance: "100px",
+// });
