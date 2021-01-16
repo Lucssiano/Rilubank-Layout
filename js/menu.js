f43a691ca687db8ad176368be74a1c528b555226
Luciano Riente <lucianotomasriente@gmail.com>
2021-01-16 20:56:16 -0300

    Se mejoró el header y la parte principal , además se agregó una nueva sección de 'cajas'

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-/* ### VARIABLES #### */
+/* ### VARIABLES ### */
 
 // Botón hamburguesa
 const burger = document.querySelector(".menu-button");
@@ -11,40 +11,50 @@ const headerContainer = document.querySelector(".header-container");
 // Listas del nav
 const listNav = document.querySelector(".li-header"); // Solo toma la primer lista, las otras no (?)
 
-/* ### FUNCIONES #### */
+/* ### FUNCIONES ### */
 
 // Abrir el menú de navegación en mobile
 burger.addEventListener("click", () => {
-  nav.classList.toggle("burger-action");
+  // nav.classList.toggle("burger-action");
+  // Se le da la clase de active para hacer un movimiento de la posición absoluta para la transición
+  nav.classList.toggle("active");
 });
 
-// Si se presiona la tecla ESCAPE se cierra el menú de navegación
+// Si se presiona la tecla ESCAPE se cierra el menú de navegación en mobile
 function escPushed() {
   teclaEsc = event.keyCode;
   if (teclaEsc == 27) {
-    nav.classList.remove("burger-action");
+    // nav.classList.remove("burger-action");
+    nav.classList.remove("active");
   }
 }
 
-// Si se presiona en alguna parte que no sea el header se cierra el menú de navegación
+// Si se presiona en alguna parte que no sea el header se cierra el menú de navegación en mobile
 window.addEventListener("click", (e) => {
-  console.log(e.target);
   if (
-    nav.classList.contains("burger-action") &&
+    nav.classList.contains("active") &&
     e.target !== header &&
     e.target !== headerContainer &&
     e.target !== listNav &&
     e.target !== burger
   ) {
-    nav.classList.remove("burger-action");
+    // nav.classList.remove("burger-action");
+    nav.classList.remove("active");
   }
 });
 
 // Inmediatamente que se presione la tecla ESCAPE se cierra el menú de navegación
 window.onkeydown = escPushed;
 
-// Scroll reveals
+/* ### Scroll reveals ### */
+
 // window.sr = ScrollReveal();
+
+// sr.reveal(".burguer-action", {
+//   duration: 3000,
+//   origin: "top",
+//   distance: "1000px",
+// });
 
 // sr.reveal(".arrow-container", {
 //   duration: 2000,
