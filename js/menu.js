b4779eb9134f83727e310895e11bce2d79e4c4ca
Luciano Riente <lucianotomasriente@gmail.com>
2021-01-25 19:21:26 -0300

    Se agrega footer , nueva sección y el cambio del botón burguer a una X

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+// ###### VARIABLES ###### //
+
 /* ### HEADER ### */
 // Botón hamburguesa
 const burger = document.querySelector(".menu-button");
@@ -8,11 +10,50 @@ const header = document.querySelector(".main-header");
 // Contenedor del header
 const headerContainer = document.querySelector(".header-container");
 // Listas del nav
-// const listNav = document.getElementsByClassName("li-header"); // Solo toma la primer lista, las otras no (?)
+// const listNav = document.querySelectorAll(".li-header"); // Solo toma la primer lista, las otras no (?)
+
+/* ### PREGUNTAS FRECUENTES ### */
+// Flecha para arriba (pregunta sin respuesta abierta)
+const upQuestionArrow = document.querySelectorAll(".fa-chevron-up");
+// Flecha para abajo (pregunta con respuesta abierta)
+const downQuestionArrow = document.querySelectorAll(".fa-chevron-down");
+// Caja de cada pregunta
+const questionBox = document.querySelectorAll(".frequently-question-box");
+// Respuesta de la pregunta
+const answerText = document.querySelectorAll(".answer-text");
+
+/* ### USER CARDS ### */
+// Flechas de las cartas para pasar las opiniones de los usuarios
+const arrowCards = document.getElementsByClassName("arrow-card");
+// Botones para pasar las cards (además de las flechas)
+const buttonSlide = document.querySelectorAll(".btn-slide");
+// Caja de opinion de cada usuario
+const userOpinionBox = document.getElementsByClassName("user-opinion-box");
+// Índice para los botones y las flechas
+let j = 0;
+
+// ###### FUNCIONES ###### //
+
+/* ### HEADER ### */
+// Para que el header cambie cuando se scrollea //
+// Ubicación principal de la página (cuando está arriba de todo)
+let principalUbication = window.pageYOffset;
+window.onscroll = function () {
+  // Cuando se empieza a scrollear se toma el valor del scroll
+  let actualUbication = window.pageYOffset;
+  // Si se scrollea un poco se le aplica la sombra al header
+  if (principalUbication < actualUbication) {
+    document.querySelector(".main-header").style.boxShadow = "2px 7px 7px -2px rgba(0, 0, 0, 0.35)";
+  } else {
+    document.querySelector(".main-header").style.boxShadow = "none";
+  }
+};
 
 // Abrir el menú de navegación en mobile
 burger.addEventListener("click", () => {
   nav.classList.toggle("active");
+  // Clase para que el boton burguer sea una x
+  burger.classList.toggle('bx-x');
 });
 
 // Si se presiona la tecla ESCAPE se cierra el menú de navegación (creo que está al dope pq las compu son de más de 768px)
@@ -34,15 +75,6 @@ window.addEventListener("click", (e) => {
 /* ######################### /*
 
 /* ### USER CARDS ### */
-// Flechas de las cartas para pasar las opiniones de los usuarios
-const arrowCards = document.getElementsByClassName("arrow-card");
-// Botones para pasar las cards (además de las flechas)
-const buttonSlide = document.querySelectorAll(".btn-slide");
-// Caja de opinion de cada usuario
-const userOpinionBox = document.getElementsByClassName("user-opinion-box");
-// Índice para los botones y las flechas
-let j = 0;
-
 // Saca la carta activa y el color del botón activo
 function removeProperties() {
   userOpinionBox[j].classList.remove("active-card");
@@ -86,56 +118,42 @@ for (let l = 0; l < buttonSlide.length; l++) {
     addProperties();
   };
 }
-/* ######################### /*
+// ######################### //
 
 /* ### PREGUNTAS FRECUENTES ### */
-// Flecha para arriba (pregunta sin respuesta abierta)
-const upQuestionArrow = document.querySelectorAll(".fa-chevron-up");
-// Flecha para abajo (pregunta con respuesta abierta)
-const downQuestionArrow = document.querySelectorAll(".fa-chevron-down");
-// Caja de cada pregunta
-const questionBox = document.querySelectorAll(".frequently-question-box");
-// Respuesta de la pregunta
-const answerText = document.querySelectorAll(".answer-text");
-
 // Dependiendo de la pregunta que se presione se ve la respuesta correspondiente
 for (let p = 0; p < upQuestionArrow.length; p++) {
   questionBox[p].onclick = function () {
     if (downQuestionArrow[p].classList.contains("icon-change")) {
       upQuestionArrow[p].classList.add("icon-change");
       downQuestionArrow[p].classList.remove("icon-change");
-      answerText[p].classList.remove("display-block");
+      answerText[p].classList.add("display-block");
     } else {
       upQuestionArrow[p].classList.remove("icon-change");
       downQuestionArrow[p].classList.add("icon-change");
-      answerText[p].classList.add("display-block");
+      answerText[p].classList.remove("display-block");
     }
   };
-  // Por si se quiere hacer con cada flecha
-  // upQuestionArrow[p].onclick = function () {
-  //   upQuestionArrow[p].classList.remove("icon-change");
-  //   downQuestionArrow[p].classList.add("icon-change");
-  //   answerText[p].classList.add("display-block");
-  // };
-  // downQuestionArrow[p].onclick = function () {
-  //   upQuestionArrow[p].classList.add("icon-change");
-  //   downQuestionArrow[p].classList.remove("icon-change");
-  //   answerText[p].classList.remove("display-block");
-  // };
 }
 
-/* ### Scroll reveals ### */
+// Por si se quiere hacer con cada flecha
+// upQuestionArrow[p].onclick = function () {
+//   upQuestionArrow[p].classList.remove("icon-change");
+//   downQuestionArrow[p].classList.add("icon-change");
+//   answerText[p].classList.add("display-block");
+// };
+// downQuestionArrow[p].onclick = function () {
+//   upQuestionArrow[p].classList.add("icon-change");
+//   downQuestionArrow[p].classList.remove("icon-change");
+//   answerText[p].classList.remove("display-block");
+// };
 
+/* ### Scroll reveals ### */
 // window.sr = ScrollReveal();
 
-// sr.reveal(".burguer-action", {
-//   duration: 3000,
-//   origin: "top",
-//   distance: "1000px",
+// ScrollReveal().reveal(".answer-text", {
+//   duration: 1000,
+//   delay: 375 ,
+//   reset: true
 // });
-
-// sr.reveal(".arrow-container", {
-//   duration: 2000,
-//   origin: "bottom",
-//   distance: "100px",
-// });
+// ScrollReveal().reveal('.answer-text', { delay: 500 });
